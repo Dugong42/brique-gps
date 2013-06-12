b344e75b1fb0e6f201c470d6252cdf857cfb8754
Julien Nozais <julien.nozais@gmail.com>
2013-06-12 23:14:22 +0200

    Added mean refresh rate display

--- gps/gps.ino
+++ gps/gps.ino
@@ -35,13 +35,20 @@ const unsigned long DELAYBUTTON = 250;
 // Nautical mile per hour (knot) in meters per second (m/s)
 const double KNOT_CONV = 0.514444444;
 
+// Number of cycles to do the refresh rate mean on
+const int CYCLE_NUMBER = 20;
+
 // Printed text
-const char* MSG_MENUS[5]={"Distance", "Position", "Vitesse", "Stats", "Temps"};
+const char* MSG_MENUS[6]={"Distance", "Position", "Vitesse", "Stats 1", "Stats 2", "Temps"};
 
 // Init external components
 LCDhandler lcd;
 GPShandler gps;
+
+// Declaration of used variables
 unsigned long timer;
+unsigned long curTimer, prevTimer; // Timers used to do a mean of the gps refresh rate
+int meanTick=0;
 int affichage;
 //SDmgmt sdCard;
 
@@ -49,7 +56,9 @@ int affichage;
 // Initialisation
 void setup() {
     timer=millis();
-
+    curTimer=millis();
+    prevTimer=curTimer;
+    
     //voir exemple arduino SD pour la raison de cette ligne
     pinMode(10, OUTPUT);
 
@@ -123,8 +132,14 @@ void printInfos() {
             lcd.printline(String(gps.getFailed()),0);
             lcd.printline(String(gps.getSentences())+" "+String(gps.getChars()),1);
             break;
-
+            
         case 4 :
+            // Show Stat
+            lcd.printline(String((curTimer-prevTimer)/CYCLE_NUMBER)+"ms",0);
+            lcd.cls(1);
+            break;
+
+        case 5 :
             // Show Date
             lcd.printline(String(gps.getDate()),0);
             lcd.printline(String(gps.getTime()),1);
@@ -199,7 +214,13 @@ void handleButtons() {
 void loop() {
     handleButtons();
     printInfos();
+    
+    meanTick=(meanTick+1)%CYCLE_NUMBER;
     gps.refreshData(lcd);
+    if (meanTick==0) {
+      prevTimer=curTimer;
+      curTimer=millis();
+    }
     //Assuming we write everything, data is logged here
     //TODO Do we write everything ? If no, what do we check ?
     //sdCard.writeCoordinates (gps.getLat(), gps.getLon(), gps.getDate(), gps.getTime(), gps.getSpeed());
